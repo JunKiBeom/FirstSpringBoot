fe5fa43271b95c3e37b765a5756a35faa504ed72
JunKiBeom <dona0408@gmail.com>
2025-01-26 16:22:36 +0900

    modify 기능 추가

--- src/main/java/com/example/demo/domain/wiseSaying/entitiy/WiseSaying.java
+++ src/main/java/com/example/demo/domain/wiseSaying/entitiy/WiseSaying.java
@@ -2,8 +2,10 @@ package com.example.demo.domain.wiseSaying.entitiy;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class WiseSaying {
     private int id;
